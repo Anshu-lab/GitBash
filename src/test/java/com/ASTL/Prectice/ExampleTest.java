86fc207125e370ad06265db32f5a71eb20beeb71
Anshu-lab <anshuramangpt@gmial.com>
2023-09-28 16:17:23 +0530

    Raman commit

---
+++ src/test/java/com/ASTL/Prectice/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.ASTL.Prectice;
+
+import org.testng.annotations.Test;
+
+public class ExampleTest {
+	@Test
+	public void display()
+	{
+		System.out.println("--DIsplay Message--");
+	}
+
+}
