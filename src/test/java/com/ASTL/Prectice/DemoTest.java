9f0823605f34ce51d16acc49c42c2ae37477bc09
Anshu-lab <anshuramangpt@gmail.com>
2023-09-28 13:54:58 +0530

    first commit

---
+++ src/test/java/com/ASTL/Prectice/DemoTest.java
@@ -0,0 +1,14 @@
+package com.ASTL.Prectice;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void displayMessage()
+	{
+		System.out.println("--Demo Message--");
+		System.out.println("-----------------");
+		System.out.println("-----------------");
+	}
+
+}
